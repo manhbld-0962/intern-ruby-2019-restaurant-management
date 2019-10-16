8de1a28bb4f0913f997712869f6f9d76c2937119
lemanh <le.duc.manhb@sun-asterisk.com>
2019-10-16 11:33:21 +0700

    edit and update user

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en/ do
     root to: "static_pages#home"
-    get "static_pages/help"
+    get "/help", to: "static_pages#help"
     devise_for :users, controllers: {
       sessions: "users/sessions",
       registrations: "users/registrations"
