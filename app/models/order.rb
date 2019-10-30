d1da58d33506cf99625ad529e884e278c4a43ab5
lemanh <le.duc.manhb@sun-asterisk.com>
2019-10-30 16:57:08 +0700

    edit orders

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,11 @@
 class Order < ApplicationRecord
+  ORDER_PARAMS = %i(number).freeze
+
   belongs_to :food
   belongs_to :booking
+
+  delegate :name, to: :food, prefix: :food
+  delegate :titleize, to: :food_name, prefix: true
 
   validates_uniqueness_of :booking_id, scope: :food_id
   validates :food_id, presence: true, on: :create
