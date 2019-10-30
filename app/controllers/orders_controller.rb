3daa431776949f6aade20cc42522d60d41234445
lemanh <le.duc.manhb@sun-asterisk.com>
2019-10-30 16:57:08 +0700

    create orders

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,49 @@
+class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :load_order, only: %i(edit update destroy)
+  before_action :load_booking, only: %i(index create)
+
+  def index
+    @pagy, @orders = pagy(@booking.orders, items: Settings.pages.page_number)
+  end
+
+  def new
+    @order = Order.new
+  end
+
+  def create
+    @order = @booking.orders.new order_params
+    @order.food_id = params[:order][:food_id]
+
+    if @order.save
+      flash[:success] = t("messages.create_success", name: @order.food_name_titleize)
+      redirect_to booking_orders_path(@booking.id)
+    else
+      flash.now[:danger] = t "messages.create_failed"
+      render :new
+    end
+  end
+
+  private
+  def load_order
+    @order = Order.find_by id: params[:id]
+
+    return if @order
+
+    flash[:danger] = t("messages.not_exists", name: Order.name)
+    redirect_to show_booking_path
+  end
+
+  def load_booking
+    @booking = Booking.find_by id: params[:booking_id]
+
+    return if @booking
+
+    flash[:danger] = t("messages.not_exists", name: Booking.name)
+    redirect_to show_booking_path
+  end
+
+  def order_params
+    params.require(:order).permit Order::ORDER_PARAMS
+  end
+end
