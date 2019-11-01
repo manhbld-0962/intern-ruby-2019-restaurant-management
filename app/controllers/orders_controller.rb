f6a21c61715860e6e9823a26fc22204b9aeca83f
lemanh <le.duc.manhb@sun-asterisk.com>
2019-11-01 10:43:29 +0700

    create, edit, delete order

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,62 @@
+class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :load_order, only: %i(edit update destroy)
+  before_action :load_booking, only: %i(index create)
+
+  def index
+    @pagy, @orders = pagy(@booking.orders, items: Settings.pages.page_number)
+  end
+
+  def new
+    @order = Order.new
+  end
+
+  def create
+    @order = @booking.orders.new order_params
+    @order[:food_id] = params[:order][:food_id]
+
+    if @order.save
+      flash[:success] = t("messages.create_success", name: @order.food_name_titleize)
+      redirect_to booking_orders_path(@booking.id)
+    else
+      flash.now[:warning] = t "messages.create_failed"
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @order.update_attributes order_params
+      flash[:success] = t("messages.update_success", name: @order.food_name_titleize)
+      redirect_to booking_orders_path(@order.booking.id)
+    else
+      flash.now[:warning] = t "messages.update_failed"
+      render :edit
+    end
+  end
+
+  private
+  def load_order
+    @order = Order.find_by id: params[:id]
+
+    return if @order
+
+    flash[:warning] = t("messages.not_exists", name: Order.name)
+    redirect_to show_booking_path
+  end
+
+  def load_booking
+    @booking = Booking.find_by id: params[:booking_id]
+
+    return if @booking
+
+    flash[:warning] = t("messages.not_exists", name: Booking.name)
+    redirect_to show_booking_path
+  end
+
+  def order_params
+    params.require(:order).permit Order::ORDER_PARAMS
+  end
+end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,16 @@
 class Order < ApplicationRecord
+  ORDER_PARAMS = %i(number).freeze
+
   belongs_to :food
   belongs_to :booking
+
+  delegate :name, to: :food, prefix: :food
 
   validates_uniqueness_of :booking_id, scope: :food_id
   validates :food_id, presence: true, on: :create
   validates :number, presence: true, numericality: {only_integer: true}
+
+  def food_name_titleize
+    self.food_name.titleize
+  end
 end
